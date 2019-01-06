ae18cccf7e015284c8ee497a6152f19cc43ee2c3
Narita1910 <43438380+Narita1910@users.noreply.github.com>
2019-01-06 22:51:39 +0900

    Update main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
 	int count=0,i;
 	char str[100];
 	int a[3], num = 0, max;
-	unsigned long x,y;
+	unsigned short x,y;
 	while (fgets(str, sizeof(str), stdin)) {
 		a[count]= atoi(str);  
 		count++;
@@ -16,10 +16,10 @@ int main(int argc, char *argv[])
 			max = a[1];
 			num = a[2];
 			for (i = 0; i < num; i++) {
-				x = (48271*x)& (2147483647-1);
+				x = (48271*x)& (2147483647);
 				//printf("%d\n", x);
 				//x= x / ((0x7fffffff + max - 1) / max) + 1;
-				y = x / ((0x7fffffff + max-1) / max-1) + 1;
+				y = x / ((0x7fffffff + max-1) / max) + 1;
 				printf("%d\n", y);
 			}
 		}
